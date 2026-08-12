1143d836904c120f45dc9a31366c281fa07bbf55
Isa <Isss>
2026-08-12 18:01:44 +0000

    add /db endpoint to check PostgreSQL connection

--- backend/app.py
+++ backend/app.py
@@ -1,4 +1,6 @@
 from flask import Flask
+import psycopg2
+import os
 app = Flask(__name__)
 @app.route('/')
 def home():
@@ -6,4 +8,19 @@ def home():
 @app.route('/health')
 def health():
     return "OK"
+@app.route('/db')
+def database():
+    try:
+        conn = psycopg2.connect(
+            host=os.getenv("DB_HOST"),
+            port=os.getenv("DB_PORT"),
+            user=os.getenv("DB_USER"),
+            password=os.getenv("DB_PASSWORD"),
+            database="postgres"
+        )
+        conn.close()
+        return "Postgresql is OK"
+    except Exception as e:
+        return "Postgres connection is failed"
+
 app.run(host="0.0.0.0", port=5000)
