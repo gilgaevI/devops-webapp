cc177031cbc12179e412861970532828cdb89626
Isa <Isss>
2026-08-14 20:53:37 +0000

    feat: add JSON for /servers endpoint

--- backend/app.py
+++ backend/app.py
@@ -1,6 +1,6 @@
 import os
 import psycopg2
-from flask import Flask
+from flask import Flask, jsonify
 app = Flask(__name__)
 def get_db_connection():
     return psycopg2.connect(
@@ -42,6 +42,30 @@ def database():
         return "Postgresql is OK", 200
     except Exception as e:
         return "Postgres connection failed", 500
+@app.route("/servers")
+def get_servers():
+    conn = psycopg2.connect(
+        host=os.getenv("DB_HOST"),
+        port=os.getenv("DB_PORT"),
+        user=os.getenv("DB_USER"),
+        password=os.getenv("DB_PASSWORD"),
+        database="postgres"
+    )
+    cursor = conn.cursor()
+    cursor.execute("SELECT * FROM servers;")
+    servers = cursor.fetchall()
+    cursor.close()
+    conn.close()
+    result = []
+    for server in servers:
+        result.append({
+            "id": server[0],
+            "name": server[1],
+            "status": server[2],
+            "cpu": server[3],
+            "ram": server[4]
+        })
+    return jsonify(result)
 if __name__ == '__main__':
     create_table()
     app.run(host="0.0.0.0", port=5000)
